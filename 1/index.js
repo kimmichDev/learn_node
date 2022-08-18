86040d25c16ad6ff2ed0ebc37f788a3b60ea0de2
kimmichDev <myueway98@gmail.com>
2022-08-19 03:57:52 +0630

    post encoded params

--- 1/index.js
+++ 1/index.js
@@ -1,25 +1,36 @@
 let http = require("http");
 let url = require("url");
+// let querystring = require(url.URLSearchParams);
+require("dotenv").config();
+
+let responder = (req, res, payload) => {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(payload);
+}
 
 let routes = {
     GET: {
         "/": (req, res) => {
-            res.writeHead(200, { "Content-type": "text/html" });
-            res.end("<h1>Get and / route</h1>");
+            responder(req, res, `<h1>Get and / route</h1>`)
         },
         "/home": (req, res) => {
-            res.writeHead(200, { "Content-type": "text/html" });
-            res.end("<h1>Get and /home route</h1>");
+            responder(req, res, `<h1>Get and /home route</h1>`)
         },
     },
     POST: {
-        "/post": () => (req, res) => {
-            res.writeHead(200, { "Content-type": "text/html" });
-            res.end("<h1>Post  and /post route</h1>");
+        "/post": (req, res) => {
+            // responder(req, res, `<h1>Post and /post route</h1>`);
+            let body = "";
+            req.on("data", data => body += data);
+            req.on("end", () => {
+                let postParams = new URLSearchParams(body);
+                // postParams.set("a", "b");
+                console.log(postParams); //querystring.parse
+                res.end();
+            });
         },
-        "/comment": () => (req, res) => {
-            res.writeHead(200, { "Content-type": "text/html" });
-            res.end("<h1>Post  and /comment route</h1>");
+        "/comment": (req, res) => {
+            responder(req, res, `<h1>Post and /comment route</h1>`)
         },
     },
     NA: (req, res) => {
@@ -28,18 +39,22 @@ let routes = {
     }
 }
 
+let server = http.createServer((req, res) => {
+    let urlObj = url.parse(req.url, true);
+    let query = urlObj.query;
+    let resolveRoute = routes[req.method][urlObj.pathname];
+    resolveRoute !== null && resolveRoute !== undefined ? resolveRoute(req, res) : routes.NA(req, res);
+
+});
+
+server.listen(process.env.PORT, () => console.log("server is running at " + process.env.PORT));
+
+
+
+
 // let start = (req, res) => {
 //     routes[req.method][req.url]();
 
 //     // res.end("method is " + req.method);
 //     // res.end("url is " + req.url);
 // }
-
-let server = http.createServer((req, res) => {
-    let urlObj = url.parse(req.url);
-    let resolveRoute = routes[req.method][urlObj.pathname];
-    resolveRoute !== null && resolveRoute !== undefined ? resolveRoute(req, res) : routes.NA(req, res);
-
-});
-
-server.listen(3000, () => console.log('server is running at port 3000'));
